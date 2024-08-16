a7d7383577d43cd7202e54a87df593f90df2005a
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-16 18:18:05 +0530

    Simple servlet

---
+++ main/java/com/greeting/GreetingServlet.java
@@ -0,0 +1,39 @@
+package com.greeting;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="greeting",value = "/greeting")
+public class GreetingServlet extends HttpServlet{
+
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message="Hello, ";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h3>" + message + request.getParameter("userName") + "</h3>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h3>" + message + request.getParameter("userName") + "</h3>");
+        out.println("</body></html>");
+    }
+}
---
+++ main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f4f4f4;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+  </style>
+</head>
+<body>
+<%--<h1>"Hello World!"</h1>--%>
+<%--<a href="hello-servlet">Hello Servlet</a>--%>
+</body>
+</html>
